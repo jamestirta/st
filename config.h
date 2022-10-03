2d7a01cf46b627fe279688c583e2d7bf2411c425
James Halim <tirtajames45@gmail.com>
2022-10-03 09:09:56 +0700

    added scrollback and mousescrollback

--- config.h
+++ config.h
@@ -16,7 +16,7 @@ static int borderpx = 3;
  * 4: value of shell in /etc/passwd
  * 5: value of shell in config.h
  */
-static char *shell = "/bin/sh";
+static char *shell = "/usr/bin/zsh";
 char *utmp = NULL;
 /* scroll program: to enable use a string like "scroll" */
 char *scroll = NULL;
@@ -179,10 +179,12 @@ static uint forcemousemod = ShiftMask;
 static MouseShortcut mshortcuts[] = {
 	/* mask                 button   function        argument       release */
 	{ XK_ANY_MOD,           Button2, selpaste,       {.i = 0},      1 },
-	{ ShiftMask,            Button4, ttysend,        {.s = "\033[5;2~"} },
-	{ XK_ANY_MOD,           Button4, ttysend,        {.s = "\031"} },
-	{ ShiftMask,            Button5, ttysend,        {.s = "\033[6;2~"} },
-	{ XK_ANY_MOD,           Button5, ttysend,        {.s = "\005"} },
+	/* { ShiftMask,            Button4, ttysend,        {.s = "\033[5;2~"} }, */
+	/* { XK_ANY_MOD,           Button4, ttysend,        {.s = "\031"} }, */
+	/* { ShiftMask,            Button5, ttysend,        {.s = "\033[6;2~"} }, */
+	/* { XK_ANY_MOD,           Button5, ttysend,        {.s = "\005"} }, */
+	{ ShiftMask,            Button5, kscrolldown,    {.i =  1} },
+	{ ShiftMask,            Button4, kscrollup,   	 {.i =  1} },
 };
 
 /* Internal keyboard shortcuts. */
@@ -202,7 +204,8 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_V,           clippaste,      {.i =  0} },
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
-	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
+	{ ShiftMask,            XK_Page_Up,    	kscrollup,      {.i =  0} },
+	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i =  0} },
 };
 
 /*
